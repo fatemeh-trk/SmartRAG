d2b3d6d82f6cd753ce1a1833258d20f9b58ee107
fatemeh torkamani <fatemeh.torkamani.git@gmail.com>
2026-07-27 19:54:20 -0400

    feat: add source citation support for RAG responses

---
+++ SRC/context_builder.py
@@ -0,0 +1,31 @@
+def build_context(results):
+    document = results["documents"]
+    if document and document[0] :
+        
+            docs = document[0]
+            metadata = results["metadatas"][0]
+            
+
+            zipped_context = zip(docs , metadata)
+            context_parts = []
+            sources = []
+            for doc_ , meta_data in zipped_context:
+        
+                file_name = meta_data["file_name"]
+                chunk_index = meta_data["chunk_index"]
+                sources.append({"chunk_index": chunk_index,
+                               "file_name":file_name
+                                })
+                formatted_chunk = f"""[chunk {chunk_index}]
+                            Source: {file_name}
+                            
+                            {doc_}"""
+                context_parts.append(formatted_chunk)
+            context = "\n-----------------------------\n".join(context_parts)
+
+        
+            return context,sources
+
+     
+    else:
+        return "",[]
